e669e4fe83d11cba91f6cf3152ab95b3d247fa12
Sebastián de los Ángeles <luissebastianangeles@gmail.com>
2023-04-19 15:34:21 -0300

    modificaciones menores

--- Linux/autorespaldo.sh
+++ Linux/autorespaldo.sh
@@ -16,10 +16,21 @@ mapeo "$rango"
 mac_server=$(cat "$ruta_local/config/dispositivo.txt")
 ip_server=$(buscar_h "$rango" "$mac_server")
 
-notify-send -t 3000 -i "$ruta_local/img/red_Freepik.png" "Sistema de Respaldo Automático" "Servidor de respaldos ubicado en la IP:$ip_server"
-
+if [ ${#ip_server} -gt 6 ]; then
+    notify-send -t 3000 -i "$ruta_local/img/red_Freepik.png" "Sistema de Respaldo Automático" "Servidor de respaldos ubicado en la IP:$ip_server"    
 
 #2 - Montar la carpeta de respaldos
 #3 - Iniciar el respaldo
 #4 - Generar informe
-#5 - Desmontar carpeta de respaldos
+#5 - Desmontar carpeta de respald
+
+else
+#ERROR 1.1 - No se puede ubicar al servidor de respaldos 
+    notify-send -t 3000 "Sistema de Respaldo Automático" "ERROR: No se pudo continuar. No se pudo ubicar al servidor de respaldos"
+    registro "ERROR" "No se pudo encontrar al servidor de respaldos. "
+fi
+
+
+
+
+os
