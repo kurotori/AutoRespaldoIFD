c7ab989447ed3796b673bf2402b6292ec067b1f6
Sebastián de los Ángeles <luissebastianangeles@gmail.com>
2023-05-08 15:49:03 -0300

    avances: mejora de notificacion,inicio de montaje

--- Linux/autorespaldo.sh
+++ Linux/autorespaldo.sh
@@ -7,6 +7,9 @@ source "$ruta_local/red.sh"
 # Permite redirigir las notificaciones al monitor principal
 export DISPLAY=':0.0'
 
+### Variables Auxiliares ###
+mensajeTit="Sistema de Respaldo Automático"
+
 #1 - Ubicar el servidor de respaldos
 
 interfaz=$(cat "$ruta_local/config/interfaz.txt")
@@ -17,17 +20,23 @@ mac_server=$(cat "$ruta_local/config/dispositivo.txt")
 ip_server=$(buscar_h "$rango" "$mac_server")
 
 if [ ${#ip_server} -gt 6 ]; then
-    notify-send -t 3000 -i "$ruta_local/img/red_Freepik.png" "Sistema de Respaldo Automático" "Servidor de respaldos ubicado en la IP:$ip_server"    
+    
+    mensCuerpo="Servidor de respaldos ubicado en la IP:$ip_server"
+    notify-send -t 3000 -i "$ruta_local/img/red_Freepik.png" "$mensajeTit" "$mensCuerpo"
 
 #2 - Montar la carpeta de respaldos
+    mount -t cifs //"${ip_servidor}"/respaldos /home/"$USER"/servidorR
+    sleep 1
+
 #3 - Iniciar el respaldo
 #4 - Generar informe
-#5 - Desmontar carpeta de respald
+#5 - Desmontar carpeta de respaldos
 
 else
 #ERROR 1.1 - No se puede ubicar al servidor de respaldos 
-    notify-send -t 3000 "Sistema de Respaldo Automático" "ERROR: No se pudo continuar. No se pudo ubicar al servidor de respaldos"
-    registro "ERROR" "No se pudo encontrar al servidor de respaldos. "
+    mensCuerpo="No se pudo encontrar al servidor de respaldos."
+    notify-send -t 3000 "$mensajeTit" "$mensCuerpo" 
+    registro "ERROR" "No se pudo encontrar al servidor de respaldos."
 fi
 
 
